1480a6f3ad5974019334809b3d3e6be7c830121e
Magicalobsidian <1191692359@qq.com>
2022-10-29 21:32:06 +0800

    添加项目文件。

---
+++ PowerKA3005PDEMO/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace PowerKA3005PDEMO.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "PowerController";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
